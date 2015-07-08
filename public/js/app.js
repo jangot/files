b73895d9a7ad7c1fe54677db78e8c5b90ce0df30
jangot <pulin.pavel@gmail.com>
2015-07-09 00:58:40 +0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,4 @@ define([
 ], function(listWidget) {
     var list = listWidget(document.querySelector('html'));
     list.createFolder();
-
-    //document
-    //    .querySelector('body')
-    //    .addEventListener('drop', function(e) {
-    //        e.stopPropagation();
-    //        e.preventDefault();
-    //
-    //    });
 });
