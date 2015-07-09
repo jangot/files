43b5810864e30d5f383e39d8a27053e5e3fe3811
jangot <pulin.pavel@gmail.com>
2015-07-09 15:27:04 +0300

    Update hash.js

--- public/js/lib/hash.js
+++ public/js/lib/hash.js
@@ -3,7 +3,9 @@ define([
 ], function() {
     return function(string) {
         var hash = 0, i, chr, len;
-        if (string.length == 0) return hash;
+        if (string.length == 0) {
+            return hash;
+        }
         for (i = 0, len = string.length; i < len; i++) {
             chr   = string.charCodeAt(i);
             hash  = ((hash << 5) - hash) + chr;
